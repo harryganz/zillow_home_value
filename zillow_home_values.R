a2894841a4af047bb602f5c4e6d05df5d9d2da1e
Harry Ganz <hganz@harryganz.com>
2024-07-21 21:18:15 -0400

    Creates gif

--- zillow_home_values.R
+++ zillow_home_values.R
@@ -3,8 +3,9 @@ library(tidyverse) |> suppressPackageStartupMessages()
 library(tigris)
 library(scales)
 library(sp)
+library(stringr)
+library(gganimate)
 library(lubridate)
-library(magick)
 
 setwd('~/projects/zillow_home_values')
 
@@ -12,12 +13,9 @@ setwd('~/projects/zillow_home_values')
 home_values <- read_csv('./County_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv') |>
   select(RegionID, RegionName, StateName, StateCodeFIPS, MunicipalCodeFIPS, starts_with('20')) |>
   pivot_longer(cols = starts_with('20'), names_to = 'Date', values_to = 'ZHVI') |>
+  filter(str_sub(Date, 6) == '01-31') |>
   mutate(Date = as.Date(Date)) |>
-  filter(Date >= as.Date('2010-01-01')) |>
-  group_by(floor_date(Date, 'year')) |>
-  arrange(Date) |>
-  filter(Date == first(Date)) |>
-  ungroup()
+  filter(Date > as.Date('2010-01-01'))
 
 # Get State data
 st <- tigris::states(year = 2021, cb = TRUE)
@@ -66,35 +64,27 @@ trans <- function(sf, degree = 0, scale = 1.0 , dx = 0, dy = 0) {
 ak_trans <- function(x)trans(x, -10, 0.4, 0.75e6, -5.2e6)
 hi_trans <- function(x)trans(x, 0, 1, 3.5e6, -1.5e6)
 
-lapply(unique(home_values$Date), function(date) {
-  cu_dat <- co_home_values_cus |> filter(Date == date)
-  ak_dat <- co_home_values_ak |> filter(Date == date)
-  hi_dat <- co_home_values_hi |> filter(Date == date)
-  yr <- year(date)
-  p <- ggplot() +
-    ggtitle(yr) +
-    geom_sf(data = st_cus, fill = "grey70") +
-    geom_sf(data = ak_trans(st_ak), fill = "grey70") +
-    geom_sf(data = hi_trans(st_hi), fill = "grey70") +
-    geom_sf(data = cu_dat, aes(fill = ZHVI), linewidth = 0.05) +
-    geom_sf(data = ak_trans(ak_dat), aes(fill = ZHVI)) +
-    geom_sf(data = hi_trans(hi_dat) ,aes(fill = ZHVI)) +
-    coord_sf(crs = original_crs, xlim = c(-124, -68)) +
-    scale_fill_stepsn("Typical Home Value", breaks = seq(2e5, 1e6, 2e5), 
-                      colors = terrain.colors(5), na.value = "grey70",
-                      labels = scales::label_currency(scale_cut = cut_short_scale())) +
-    theme_void() +
-    theme(plot.title = element_text(size = unit("16", "pt"), hjust = 1.0), plot.margin = unit(rep(0.25, 4), "in"))
-  fp <- file.path(paste0('./figures/zhvi_', yr, ".png"))
-  ggsave(plot = p, filename = fp, device = "png")
-})
 
-imgs <- list.files('./figures', full.names = TRUE)
-img_animated <- lapply(imgs, image_read) |>
-  image_join() |>
-  image_animate(fps = 2)
+p <- ggplot() +
+  geom_sf(data = st_cus, fill = "grey70") +
+  geom_sf(data = ak_trans(st_ak), fill = "grey70") +
+  geom_sf(data = hi_trans(st_hi), fill = "grey70") +
+  geom_sf(data = co_home_values_cus, aes(fill = ZHVI), linewidth = 0.05) +
+  geom_sf(data = ak_trans(co_home_values_ak), aes(fill = ZHVI)) +
+  geom_sf(data = hi_trans(co_home_values_hi) ,aes(fill = ZHVI)) +
+  coord_sf(crs = original_crs, xlim = c(-124, -68)) +
+  scale_fill_stepsn("Typical Home Value", breaks = seq(2e5, 1e6, 2e5), 
+                    colors = terrain.colors(5), na.value = "grey70",
+                    labels = scales::label_currency(scale_cut = cut_short_scale())) +
+  labs(title = 'Year: {floor(frame_time)}') +
+  transition_time(year(Date)) +
+  ease_aes('linear') +
+  theme_void() +
+  theme(plot.title = element_text(size = unit("16", "pt"), hjust = 1.0), plot.margin = unit(rep(0.25, 4), "in"))
 
 
 
+animate(p, width = 1000, height = 1000, units = "px", nframes = 200, start_pause = 20, end_pause = 50)
 
+anim_save("./figures/zhvi.gif")
 
